aa4dd74daad7b932b304bb4185f7933b3cfe435c
melodymind360 <beetsy.maltby1@gmail.com>
2025-10-13 13:44:56 -0600

    First python

---
+++ lab 10-13/somwthing.py
@@ -0,0 +1,16 @@
+name ="Anthony"
+hobby="working out" 
+yearofbirth="2007"
+currentyear="2025"
+age=int(currentyear)-int(yearofbirth)
+#current notes are it is important to make sure variables are easy to read and
+# retype but are detailed enough to understand without needing a key or legend
+# Print is when it will give a certian amount and than fill out a variable with what was asked and 
+# saved earleir
+
+
+print(f"hello {name}, you are {age} years old and you like {hobby}")
+
+#f is for format and it will fill in the variables with what was asked
+# {} is for the variables to be filled in, must use the squigle bracket or it wont work, you also need add4
+# qoutations marks after the f and before the last parentasis.
